46c86793627c746cb201d8d8bbcc990134c85ffc
AlexanderAST <leha041304@gmail.com>
2023-10-24 21:05:51 +0300

    first commit

---
+++ src/main/scala/com/example/Reponse.scala
@@ -0,0 +1,3 @@
+package com.example
+
+case class Responce(status: String, msg: String)
---
+++ src/main/scala/com/example/UserRoutes.scala
@@ -0,0 +1,104 @@
+package com.example
+
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.Route
+import io.circe.syntax._
+
+import scala.concurrent.Future
+import com.example.UserRegistry._
+import akka.actor.typed.ActorRef
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.AskPattern._
+import akka.util.Timeout
+import io.circe.Encoder.AsArray.importedAsArrayEncoder
+import io.circe.generic.auto.exportEncoder
+
+//#import-json-formats
+//#user-routes-class
+class UserRoutes(userRegistry: ActorRef[UserRegistry.Command])(implicit val system: ActorSystem[_]) {
+
+  //#user-routes-class
+  import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+  import JsonFormats._
+  //#import-json-formats
+
+  // If ask takes more time than this to complete the request is failed
+  private implicit val timeout: Timeout = Timeout.create(system.settings.config.getDuration("my-app.routes.ask-timeout"))
+
+  def getUsers(): Future[Users] =
+    userRegistry.ask(GetUsers)
+  def getUser(name: String): Future[GetUserResponse] =
+    userRegistry.ask(GetUser(name, _))
+  def createUser(user: User): Future[ActionPerformed] =
+    userRegistry.ask(CreateUser(user, _))
+  def deleteUser(name: String): Future[ActionPerformed] = {
+    userRegistry.ask(DeleteUser(name, _))
+  }
+  def getCounter(): Future[Users] =
+    userRegistry.ask(GetCounter)
+
+  //#all-routes
+  //#users-get-post
+  //#users-get-delete
+  val userRoutes: Route =
+    pathPrefix("users") {
+      concat(
+        //#users-get-delete
+        pathEnd {
+          concat(
+            get {
+              complete(getUsers())
+            },
+            post {
+              entity(as[User]) { user =>
+                onSuccess(createUser(user)) { performed =>
+                  complete((StatusCodes.Created, performed.asJson.noSpaces))
+                }
+              }
+            })
+        },
+        //#users-get-delete
+        //#users-get-post
+        path(Segment) { name =>
+          concat(
+            get {
+              //#retrieve-user-info
+              rejectEmptyResponse {
+                onSuccess(getUser(name)) { response =>
+                  complete(response.maybeUser)
+                }
+              }
+              //#retrieve-user-info
+            },
+            delete {
+              //#users-delete-logic
+              onSuccess(deleteUser(name)) { performed =>
+                complete((StatusCodes.OK, performed))
+              }
+              //#users-delete-logic
+            })
+
+        })
+      //#users-get-delete
+
+    }
+
+  val counterUsers:Route =
+      pathPrefix("allUsers") {
+        get{
+          complete(getCounter())
+        }
+      }
+  //#all-routes
+  val helloRoute: Route =
+  get {
+    pathPrefix("hello") {
+
+      val result = Responce("ok","hello world").asJson.noSpaces
+      complete(StatusCodes.OK, result)
+    }
+  }
+
+  val routesResult: Route = helloRoute ~ userRoutes ~ counterUsers
+}
